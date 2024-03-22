0cae46013afc859d8d8f6d85f30be0c0d50ec5aa
Max Hewitt <mxhew20@gmail.com>
2024-03-22 16:34:09 +1300

    Initial redux exercises commit

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,14 @@
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdotesList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdotesList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,29 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdotesList = () => {
+    const anecdotes = useSelector(state => state.sort((a, b) => a.votes > b.votes ? -1 : 1 ))
+    const dispatch = useDispatch()
+
+    const vote = (id) => {
+        dispatch(voteAnecdote(id))
+    }
+
+    return (
+        <>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                <div>
+                    {anecdote.content}
+                </div>
+                <div>
+                    has {anecdote.votes}
+                    <button onClick={() => vote(anecdote.id)}>vote</button>
+                </div>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default AnecdotesList
